bd432a0fe70764ef893aea3bfe1518d473eb07f3
Chao Suo <c.suo@tu-bs.de>
2017-08-24 23:47:31 +0200

    add inventory management

--- public/header.js
+++ public/header.js
@@ -1,12 +1,13 @@
 function addHeader() {
 	var string = '<img src="logo.png" alt="logo" width="300" height="90.8">'
-			   + "<h1>BILIN Angebots- und Rechnungssystem</h1>"
+			   + "<h1>BILIN Buchhaltungssystem</h1>"
 			   + "<header>"
 			   + "<nav>"
 			   + "<ul>"
 			   + '<li><a href="index.html">Adresse</a></li>'
 			   + '<li><a href="order.html">Bestellung</a></li>'
 			   + '<li><a href="option.html">Optionen</a></li>'
+			   + '<li><a href="inventory.html">Inventarverwaltung</a></li>'
 			   + "</ul>"
 			   + "</nav>"
 			   + "</header>";
